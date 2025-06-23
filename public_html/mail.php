edb42bad6aac7ad5a0939add4b805842aaf1371b
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-06-23 23:10:00 +0530

    fix bug and show error/success message

--- public_html/mail.php
+++ public_html/mail.php
@@ -1,11 +1,10 @@
 <?php
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-$emailApiKey = getenv('EMAIL_API_KEY');
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect post data
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    $emailApiKey = getenv('EMAIL_API_KEY');
+    $host = getenv('HOST');
     
     $authKey = isset($_POST["api"]) ? trim($_POST["api"]) : null;
     
@@ -17,21 +16,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $to = filter_var($_POST["to"], FILTER_VALIDATE_EMAIL);
     $subject = htmlspecialchars($_POST["subject"]);
-    $message = htmlspecialchars($_POST["message"]);
-    $headers = "From: noreply@letagentdo.xyz\r\n";
-    $headers .= "Reply-To: noreply@letagentdo.xyz\r\n";
+    $message = filter_var($_POST['message'], FILTER_UNSAFE_RAW);
+    $headers = "From: noreply@{$host}\r\n";
+    $headers .= "Reply-To: noreply@{$host}\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
+    $sent = false;
     if ($to && $subject && $message) {
         if (mail($to, $subject, $message, $headers)) {
             echo "Email sent successfully!";
-            echo $to;
+            $sent = true;
         } else {
             echo "Failed to send email.";
         }
     } else {
         echo "Invalid input.";
     }
+    if ($sent) {
+        header("Location: ?status=success");
+        exit;
+    } else {
+        header("Location: ?status=fail");
+        exit;
+    }
+}
+?>
+
+<?php
+$status = $_GET['status'] ?? null;
+
+if ($status === 'success') {
+    echo '<p style="color:green;">Email sent successfully!</p>';
+} elseif ($status === 'fail') {
+    echo '<p style="color:red;">Failed to send email.</p>';
 }
 ?>
 
@@ -50,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" name="subject" required><br><br>
 
         <label for="subject">API key:</label><br>
-        <input type="text" name="api" required><br><br>
+        <input type="password" name="api" required><br><br>
 
         <label for="message">Message:</label><br>
         <textarea name="message" rows="6" cols="40" required></textarea><br><br>
